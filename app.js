71099375503447c225b8d3a2b9b8962a00fe663d
karishmaMohammed <karishmamohammed43@gmail.com>
2025-02-16 11:21:43 +0530

    register api

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const connectToMongoDb = require('./db');
+const userRoutes = require('./routes/user.routes');
+
+connectToMongoDb();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send('Hello from uber backend')
 
 })
+
+
+app.use('/users', userRoutes);
 
 module.exports = app
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,40 @@
 const userModel = require('../models/user.model')
+const userService = require('../services/user.service');
+const { validationResult } = require('express-validator');
 
 
-async function registerUser(req, res, next) {
-    try {
+registerUser = async (req, res, next) => {
 
-    } catch {
-
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
+
+    const { fullname, email, password } = req.body;
+
+    const isUserAlready = await userModel.findOne({ email });
+
+    if (isUserAlready) {
+        return res.status(400).json({ message: 'User already exist' });
+    }
+
+    const hashedPassword = await userModel.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
+        email,
+        password: hashedPassword
+    });
+
+    const token = user.generateAuthenticationToken();
+
+    res.status(201).json({ token, user });
+
+
 }
+
+
 
 module.exports = {
     registerUser,
